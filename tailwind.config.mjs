548212bd865650393ca5b2806aedb498d94e2816
suzza <suzza@susansilver.net>
2024-06-26 13:23:37 -0700

    add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,16 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {colors: {
+			// Your preferred accent color. Indigo is closest to Starlight’s defaults.
+			accent: colors.rose,
+			// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
+			gray: colors.slate
+		  },},
 	},
 	plugins: [starlightPlugin()],
 }
