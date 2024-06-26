488b0a8bf95eecfbb2009562e18f475510304ad9
suzza <suzza@susansilver.net>
2024-06-26 13:19:06 -0700

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://suzza.dev',
@@ -8,14 +10,13 @@ export default defineConfig({
     title: 'Site with sitemap',
     favicon: '/favicon.ico',
     title: 'Docs with Tailwind',
-		customCss: [
-				// Path to your Tailwind base styles:
-				'./src/styles/tailwind.css',
-			],
+    customCss: [
+    // Path to your Tailwind base styles:
+    './src/styles/tailwind.css'],
     title: 'Newbie-ish Guide to Astro',
     social: {
       github: 'https://github.com/susansilver',
-      mastodon: 'https://mathstodon.xyz/@suzza',
+      mastodon: 'https://mathstodon.xyz/@suzza'
     },
     sidebar: [{
       label: 'Getting Sarted',
@@ -35,7 +36,7 @@ export default defineConfig({
       autogenerate: {
         directory: 'Two'
       }
-    },{
+    }, {
       label: 'Chapter Three',
       autogenerate: {
         directory: 'Three'
@@ -77,22 +78,32 @@ export default defineConfig({
       }
     }, {
       label: 'Things to Remember',
-			items: [
-				{ label: 'Recommended Integrations', link: '/remember/integrations/' },
-				{ label: 'Check for Updates', link: '/remember/updates/' },
-        { label: 'Show Your Support', link: '/remember/support/' },
-        { label: 'Final Thoughts on Astro', link: '/remember/final-thoughts/' },
-        { label: 'Thank You & Further Resources', link: '/remember/thank-you/' },
-				// A nested group of links.
-      ],
+      items: [{
+        label: 'Recommended Integrations',
+        link: '/remember/integrations/'
+      }, {
+        label: 'Check for Updates',
+        link: '/remember/updates/'
+      }, {
+        label: 'Show Your Support',
+        link: '/remember/support/'
+      }, {
+        label: 'Final Thoughts on Astro',
+        link: '/remember/final-thoughts/'
+      }, {
+        label: 'Thank You & Further Resources',
+        link: '/remember/thank-you/'
+      }
+      // A nested group of links.
+      ]
     }, {
       label: 'Addendum',
       autogenerate: {
         directory: 'Addendum'
       }
-    }
-  ]
-  }),
-  
-	],
+    }]
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  })]
 });
