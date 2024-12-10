346e29c5bafa5e8cc7ff1151156e5d0e553c2486
suzza <suzza@susansilver.net>
2024-12-10 06:28:32 -0800

    remove twitter card

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,13 +39,6 @@ export default defineConfig({
             content: "/og-image-suzza-dev.png",
           },
         },
-        {
-          tag: "meta",
-          attrs: {
-            property: "twitter:card",
-            content: "summary_large_image",
-          },
-        },
       ],
       title: "Newbie-ish Guide to Astro",
       social: {
